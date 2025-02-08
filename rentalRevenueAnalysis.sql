43a601b6a2636223b9e51cd2135276ea15360c19
Jacob Ardoin <134813460+jhardoin@users.noreply.github.com>
2025-02-07 22:26:44 -0600

    Update and rename B - Create Function.sql to rentalRevenueAnalysis.sql

--- rentalRevenueAnalysis.sql
+++ rentalRevenueAnalysis.sql
@@ -1,9 +1,4 @@
--- Jacob Ardoin
--- Student ID: 012456585 
--- Advanced Data Management - D326
--- Rental Revenue By Film Category
-
--- B - User Defined Function From A4
+-- create_function
 CREATE OR REPLACE FUNCTION calculate_rental_revenue(rental_count BIGINT, rental_rate NUMERIC) 
 RETURNS NUMERIC AS $$
 BEGIN
@@ -12,7 +7,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 
--- C - Create Detailed Table
+--  Create Detailed Table
 CREATE TABLE detailed_category_revenue AS
 SELECT 
     c.name AS category_name, 
@@ -28,11 +23,8 @@ GROUP BY c.name, f.film_id, f.title, f.rental_rate
 ORDER BY rental_revenue DESC;
 
 
--- C - Confirm Detailed Data
-SELECT * FROM detailed_category_revenue 
 
-
--- C - Create Summary Table
+--  Create Summary Table
 CREATE TABLE summary_category_revenue AS
 SELECT 
     category_name, 
@@ -43,11 +35,7 @@ GROUP BY category_name
 ORDER BY total_revenue DESC;
 
 
--- C - Confirm Summary Data
-SELECT * FROM summary_category_revenue
-
-
--- D - Raw Data Extraction
+-- Raw Data Extraction
 SELECT 
     c.name AS category_name, 
     f.film_id, 
@@ -62,7 +50,7 @@ JOIN category c ON fc.category_id = c.category_id
 ORDER BY c.name, f.film_id;
 
 
--- E - Create Summary Update Funtion
+--  Create Summary Update Funtion
 CREATE OR REPLACE FUNCTION update_summary_table()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -81,19 +69,16 @@ END;
 $$ LANGUAGE plpgsql;
 
 
--- E - Create Trigger for Summary Update
+--  Create Trigger for Summary Update
 CREATE TRIGGER trigger_update_summary
 AFTER INSERT OR UPDATE OR DELETE ON detailed_category_revenue
 FOR EACH STATEMENT
 EXECUTE FUNCTION update_summary_table();
 
 
--- E - Trigger Test
-INSERT INTO detailed_category_revenue (category_name, film_id, title, rental_revenue)
-VALUES ('Action', 5648, 'Movie by Jake', 1000000000.00);
 
 
--- F - Create Procedure
+--  Create Procedure
 CREATE OR REPLACE PROCEDURE refresh_report_data() --pgAgent CALL
 LANGUAGE plpgsql
 AS $$
